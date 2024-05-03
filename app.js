5a1ffa95e9bb5050f02a9c032d293785b9bd86c1
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2024-05-03 10:45:12 +0400

    Set cookie on disclaimer close

--- app.js
+++ app.js
@@ -11,9 +11,45 @@ const init = () => {
 }
 
 const initDisclaimer = () => {
-	document.querySelector('#close').addEventListener('click', () => {
+	const cookieName = 'score-tracker-disclaimer';
+	
+	const hideDisclaimer = () => {
 		document.querySelector('.disclaimer').classList.add('hide');
+	}
+
+	if(getCookie(cookieName) != null) {
+		hideDisclaimer();
+	}
+
+	document.querySelector('#close').addEventListener('click', () => {
+		hideDisclaimer();
+		setCookie(cookieName, true, 1);
 	});
+}
+
+const setCookie = (name, value, days) => {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+const getCookie = (name) => {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+const deleteCookie = (name) => {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 const getSheetUrl = () => {
